7279f8394538ea4ceeb61e3a83392ce65f394863
pigeon_gugugu <892197240@qq.com>
2022-01-29 21:26:52 +0800

    feat: finish login inteface

--- rosemary/auth/login.py
+++ rosemary/auth/login.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session
 from werkzeug.security import generate_password_hash, check_password_hash
-from rosemary.extension import db
+from rosemary.extension import db, res
 from rosemary.model import User
 
 auth_login_bp = Blueprint('login', __name__, url_prefix='/auth')
@@ -12,4 +12,7 @@ def login():
     password = request.form.get("password")
     user = User.query.get(mailbox)
     if user is None or not check_password_hash(pwhash=user.password, password=password):
-        return "", 701
+        return res(), 701
+    else:
+        session["mailbox"] = mailbox
+        return res(), 200
--- rosemary/extension.py
+++ rosemary/extension.py
@@ -6,14 +6,5 @@ import jwt
 db: SQLAlchemy = SQLAlchemy()
 
 
-def make_json_response(
-        status=0,
-        message=None,
-        data=None
-):
-    response = jsonify({
-        "status": status,
-        "message": message,
-        "data": data,
-    })
-    return response
+def res(data=None):
+    return jsonify(data)
